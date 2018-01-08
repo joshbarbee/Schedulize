59569122288b312a1a85ef9a965c005915faa6ba
Josh Barbee <32991764+joshbarbee@users.noreply.github.com>
2018-01-07 23:59:03 -0500

    Add files via upload

--- roulette.py
+++ roulette.py
@@ -7,13 +7,14 @@ import datetime
 link = feedparser.parse("https://www.stxavier.org/calendar/calendar_27.rss")
 a = ""
 temp = 0
-for post in link.entries:
+ks = []
+for i, post  in enumerate(link.entries):
     a += (post.title + " " + post.description)
     b = a.replace("<br>", "\n").replace("<p>", "").replace("</p>", " ").replace("<br />", " ")
-
+    ks.append(i)
 daylist = b.split("\t\t\t\t")
 current_day = datetime.datetime.now()
 
-print(daylist[current_day.day])
+print(daylist[ks[0]])
     
     
